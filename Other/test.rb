880b605821597a052f513e1f729d4aaf5876c784
Brijesh <brijeshgpt7@gmail.com>
2015-05-10 23:44:19 +0530

    made correction

--- Other/test.rb
+++ Other/test.rb
@@ -57,17 +57,18 @@
 #1: module remove name space collision
 #2: implement mixin
 
-#
 #class Class
 #
-#
-#  def attr_validated(method_name)
-#    self.class_eval("def #{method_name};@#{method_name};end")
-#    self.class_eval("def #{method_name}=(val);@#{method_name}=val;end")
+#  def my_attr_accessor(*args)
+#    args.each do |arg|
+#      # getter
+#      self.class_eval("def #{arg};@#{arg};end")
+#      # setter
+#      self.class_eval("def #{arg}=(val);@#{arg}=val;end")
+#    end
 #  end
-#
 #end
-#
+
 #class Dog
 #  attr_validated :num_legs
 #
@@ -91,17 +92,6 @@
 #  end
 #end
 
-#class Class
-#
-#  def my_attr_accessor(*args)
-#    args.each do |arg|
-#      # getter
-#      self.class_eval("def #{arg};@#{arg};end")
-#      # setter
-#      self.class_eval("def #{arg}=(val);@#{arg}=val;end")
-#    end
-#  end
-#end
 ################################
 ################################
 #class Array
@@ -119,7 +109,7 @@
 #class CodebrahmaArray < Array
 #  def sum(*args)
 #    result = 0
-#    args.each{ |a| result +=a  }
+#    args.each{ |a| result += a  }
 #    result
 #  end
 #
@@ -140,3 +130,37 @@
 #  Array.new.sum(1,2,3,4,5,6)
 #end
 #p time_2.real
+
+########################################################
+########################################################
+#class Class
+#  def attr_validated(*args, &validator)
+#    args.each do |name|
+#      define_method("#{name}=") do |value|
+#        if block_given?
+#          raise ArgumentError, "Value '#{value}' is invalid" unless validator.call(value)
+#        end
+#
+#        instance_variable_set("@#{name}", value)
+#      end
+#
+#      define_method(name) do
+#        instance_variable_get("@#{name}")
+#      end
+#    end
+#  end
+#end
+#
+#class Dog
+#  attr_validated :num_legs do  |name|
+#    name <= 3
+#  end
+#end
+#dog = Dog.new
+#dog.num_legs = 3 # Works fine
+#p dog.num_legs
+#dog.num_legs = 27 # Raises ArgumentError
+#p dog.num_legs
+
+
+
