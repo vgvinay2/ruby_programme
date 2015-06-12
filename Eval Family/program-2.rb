696255a20d9ce6bcc573777a461f426c34104867
Brijesh <brijeshgpt7@gmail.com>
2015-06-12 17:36:33 +0530

    renamed file

---
+++ Eval Family/program-2.rb
@@ -0,0 +1,20 @@
+class EvalFamily
+
+  def eval_test
+    'Hii'
+  end
+
+end
+
+EvalFamily.instance_eval do
+
+  def using_eval_kass
+    'using_eval_ckass'
+  end
+
+end
+
+# p EvalFamily.new.using_eval_kass
+p EvalFamily.using_eval_kass
+
+
