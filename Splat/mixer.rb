17c16435732484d15b14185d5a446e14f08d4663
Brijesh <brijeshgpt7@gmail.com>
2015-08-17 18:22:09 +0530

    Update mixer.rb

--- Splat/mixer.rb
+++ Splat/mixer.rb
@@ -12,3 +12,15 @@ hello('denny', :a, :b, upcase: true) { 'block this!' }
 # args: [:a, :b]
 # options: {:upcase=>true}
 # => #<Proc:0x007fc1a10afd60>
+# See? You can splat before a required argument just fine. Splatting before any optional arguments though?
+
+# Works fine:
+def hello(name = nil, *args)
+
+# Throws SyntaxError:
+def hello(*args, name = nil)
+
+# Using several splats in a definition SyntaxErrors too:
+def hello(a, *args, b, *brgs)
+Ruby wouldn’t know where to cut off the splats, so the SyntaxError’s are well deserved.
+
