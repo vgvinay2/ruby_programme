8fff6b617552320118e1c493f996659f6fb07473
Brijesh <brijeshgpt7@gmail.com>
2015-08-25 17:22:47 +0530

    Update program-10-custum-attr-accessor.rb

--- meta-programming/program-10-custum-attr-accessor.rb
+++ meta-programming/program-10-custum-attr-accessor.rb
@@ -1,19 +1,22 @@
 module Accessor
-def my_attr_accessor(name)
-ivar_name = "@#{name}"
-define_method(name) do
-instance_variable_get(ivar_name)
+	def my_attr_accessor(name)
+	ivar_name = "@#{name}"
+		define_method(name) do
+		instance_variable_get(ivar_name)
+		end
+	
+		define_method("#{name}=") do |value|
+		p "#{ivar_name} #{value}"
+		instance_variable_set(ivar_name,value)
+		end
+	end
 end
-define_method("#{name}=") do |value|
-p "#{ivar_name} #{value}"
-instance_variable_set(ivar_name,value)
-end
-end
-end
+
 class Test
-extend Accessor
-my_attr_accessor :val
+	extend Accessor
+	my_attr_accessor :val
 end
+
 test = Test.new
 test.val = 66
 test.val = 64
