d52a8643bf3e5c1a07c944a6f19c0f2c325c07f8
Brijesh <brijeshgpt7@gmail.com>
2015-10-12 15:18:37 +0530

    :goberserk: :goberserk:

--- all-about-singleton/program-1.rb
+++ all-about-singleton/program-1.rb
@@ -1,9 +1,9 @@
-## Singleton methods
+## all-about-singleton methods
 # Since all methods are implemented and stored by the class definition,
 # it should be impossible for an object to define its own methods.
 # However, Ruby provides a way around this - you can define methods that
 # are available only for a specific object.
-# Such methods are called Singleton Methods.
+# Such methods are called all-about-singleton Methods.
 # Let us look at an example of a singleton method:
 
 class Foo
