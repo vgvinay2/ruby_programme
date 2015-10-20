238416db880515cb4db208b96cfc6e3a9ed895c3
Brijesh <brijeshgpt7@gmail.com>
2015-10-20 15:22:33 +0530

    :foggy: Typoo

--- all-about-singleton/program-1.rb
+++ all-about-singleton/program-1.rb
@@ -3,13 +3,13 @@
 # it should be impossible for an object to define its own methods.
 # However, Ruby provides a way around this - you can define methods that
 # are available only for a specific object.
-# Such methods are called all-about-singleton Methods.
+# Such methods are called singleton Methods.
 # Let us look at an example of a singleton method:
 
 class Foo
 end
 
-foo=Foo.new
+foo = Foo.new
 def foo.shout
   puts "Foo Foo Foo!"
 end
