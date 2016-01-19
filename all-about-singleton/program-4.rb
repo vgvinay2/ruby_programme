94ec5b35d6a2784a8675ae0e0e05005cd32df50b
Brijesh <brijeshgpt7@gmail.com>
2016-01-19 16:55:40 +0530

    Update program-4.rb

--- all-about-singleton/program-4.rb
+++ all-about-singleton/program-4.rb
@@ -1,7 +1,7 @@
-The instance_exec method is available on every Object, 
-and everything in Ruby is an Object, and when you use it 
-you open up the singleton_class of the object to work on. 
-Anything you can do in a Class you could do in an instance_exec.
+# The instance_exec method is available on every Object, 
+# and everything in Ruby is an Object, and when you use it 
+# you open up the singleton_class of the object to work on. 
+# Anything you can do in a Class you could do in an instance_exec.
 
 num = Object.new
 num.instance_exec {
@@ -12,4 +12,20 @@ num.instance_exec {
 num == 4
 # => false
 num == 3
+# => true
+
+# You can even use this on a Proc… which could drive people crazy if you do.
+# But just so you know that you can, here it is.
+
+prc = proc {|num| num == 4}
+prc.instance_exec { def == other; other == 3 end  }
+ 
+prc.call(4)
+# => true
+prc.call(3)
+# => false
+ 
+prc == 4
+# => false
+prc == 3
 # => true
